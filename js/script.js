c6fceb4627e7935967c11604584d1589610d9ef0
Shreyas A S <shreyas.as@techversantinfotech.com>
2024-10-23 11:33:39 +0530

    Implement basic functionality

---
+++ js/script.js
@@ -0,0 +1,38 @@
+var data = [{
+    start: 0,
+    duration: 15,
+    title: "Exercise"
+}, {
+    start: 30,
+    duration: 30,
+    title: "Plan day"
+}, {
+    start: 60,
+    duration: 15,
+    title: "Review yesterday's commits"
+}, {
+    start: 100,
+    duration: 15,
+    title: "Code review"
+}, {
+    start: 180,
+    duration: 90,
+    title: "Have lunch with John"
+}, {
+    start: 370,
+    duration: 45,
+    title: "Follow up with designer"
+}
+];
+
+const parentDiv = document.getElementById("timeline-overlay");
+
+for (let obj of data) {
+    const divNew = document.createElement("div");
+    divNew.style.position = "absolute";
+    divNew.style.backgroundColor = "lightblue";
+    divNew.style.top = obj.start + "px";
+    divNew.style.height = obj.duration + "px";
+    divNew.textContent = obj.title;
+    parentDiv.appendChild(divNew);
+}
