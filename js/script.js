3ec7c0f2ddbb54a6ee8897c6f23f7830a85e5189
Shreyas A S <shreyas.as@techversantinfotech.com>
2024-10-23 20:14:11 +0530

    Solve some issues and implement timeline nicely

--- js/script.js
+++ js/script.js
@@ -8,17 +8,21 @@ var data = [{
     title: "Travel to work"
 }, {
     start: 30,
-    duration: 30,
+    duration: 180,
     title: "Plan day"
 }, {
-    start: 60,
+    start: 35,
     duration: 15,
     title: "Review yesterday's commits"
 }, {
-    start: 100,
+    start: 27,
     duration: 15,
     title: "Code review"
 }, {
+    start: 90,
+    duration: 50,
+    title: "Cycling",
+},{
     start: 180,
     duration: 90,
     title: "Have lunch with John"
@@ -37,35 +41,34 @@ var data = [{
 }
 ];
 
-const parentDiv = document.getElementById("timeline-overlay");
+data.sort((a, b) => a.start - b.start);
 
-let pos = "right";
+let overlappingArrBak = [];
 for (let i=0; i<data.length; i++) {
-    const obj = data[i];
-    const objPrev = data[i-1];
-    const objNext = data[i+1];
-    const divNew = document.createElement("div");
-    if ((i-1 >= 0 && (objPrev.start + objPrev.duration) > obj.start) || (i+1 < data.length && (obj.start + obj.duration) > objNext.start)) {
-        divNew.style.width = "50%";
-        if (pos == "right") {
-            divNew.style.left = "50%";
-        }
-        else {
-            divNew.style.right = "50%";
-        }
+    const overlappingArr = [];
+    let j = i;
 
-        if (pos == "left") {
-            pos = "right";
+    do {
+        overlappingArr.push(i);
+        i++;
+    } while (i < data.length && (data[j].start + data[j].duration) > data[i].start);
+    i--;
+
+    for (let k=0; k<overlappingArr.length; k++) {
+        const divNew = document.createElement("div");
+        divNew.style.left = (100/overlappingArr.length * k) + "%";
+        divNew.style.top = data[overlappingArr[k]].start + "px";
+        divNew.style.height = data[overlappingArr[k]].duration + "px";
+        divNew.textContent = data[overlappingArr[k]].title;
+        const bigArr = overlappingArrBak.filter((value) => data[value].start + data[value].duration > data[overlappingArr[k]].start);
+        if (bigArr.length !== 0) {
+            divNew.style.width = ((overlappingArrBak.indexOf(bigArr[0]) / overlappingArrBak.length) * 100) + "%";
+            divNew.style.zIndex = 0;
+        } else {
+            divNew.style.width = 100/overlappingArr.length + "%";
+            divNew.style.zIndex = 1;
         }
-        else {
-            pos = "left";
-        }
+        document.getElementById("timeline-overlay").appendChild(divNew);
     }
-    else {
-        pos = "right";
-    }
-    divNew.style.top = obj.start + "px";
-    divNew.style.height = obj.duration + "px";
-    divNew.textContent = obj.title;
-    parentDiv.appendChild(divNew);
+    overlappingArrBak = [...overlappingArr];
 }
