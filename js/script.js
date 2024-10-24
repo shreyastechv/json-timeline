9933b04c15a5b3d350cea64255bc50cfe826090d
Shreyas A S <shreyas.as@techversantinfotech.com>
2024-10-24 15:28:33 +0530

    Implement best version till now

--- js/script.js
+++ js/script.js
@@ -52,6 +52,8 @@ for (let i=0; i<data.length; i++) {
     } while (i < data.length && ((data[j].start + data[j].duration) > data[i].start));
     i--;
 
+    let width = 100;
+
     for (let j=0; j<overlappingArr.length; j++) {
         const divNew = document.createElement("div");
         divNew.style.marginLeft = (100/overlappingArr.length * j) + "%";
@@ -60,15 +62,24 @@ for (let i=0; i<data.length; i++) {
         divNew.textContent = data[overlappingArr[j]].title;
         const bigArr = overlappingArrBak.filter((value) => data[value].start + data[value].duration > data[overlappingArr[j]].start);
         if (bigArr.length !== 0) {
+            if (j ==0 && overlappingArr.length > 1 && Math.max(...overlappingArrBak) + 1 == Math.min(...overlappingArr)) {
+                width = width / 2;
+            }
             if (overlappingArrBak.indexOf(bigArr[0]) == 0) {
-                const prevBigDiv = document.querySelector(`#timeline-overlay > div:nth-last-child(${overlappingArrBak.length - overlappingArrBak.indexOf(bigArr.at(-1))})`);
+                let prevBigDiv = document.querySelector(`#timeline-overlay > div:nth-last-child(${overlappingArrBak.length + j})`);
+                for (let k=overlappingArrBak.length + j; k>0; k--) {
+                    const prevDiv = document.querySelector(`#timeline-overlay > div:nth-last-child(${k})`);
+                    if (parseInt(prevDiv.style.top) + parseInt(prevDiv.style.height) > parseInt(prevBigDiv.style.top) + parseInt(prevBigDiv.style.height)) {
+                        prevBigDiv = prevDiv;
+                    }
+                }
                 console.log(prevBigDiv.textContent);
                 divNew.style.marginLeft = (parseInt(prevBigDiv.style.width) + parseInt(prevBigDiv.style.marginLeft)) + "%";
-                divNew.style.width = (((overlappingArrBak.indexOf(bigArr.at(-1))+1) / overlappingArrBak.length) * 100) + "%";
+                divNew.style.width = (((overlappingArrBak.indexOf(bigArr.at(-1))+1) / overlappingArrBak.length) * width) + "%";
                 divNew.style.zIndex = 1;
             }
             else {
-                divNew.style.width = ((overlappingArrBak.indexOf(bigArr.at(-1)) / overlappingArrBak.length) * 100) + "%";
+                divNew.style.width = ((overlappingArrBak.indexOf(bigArr.at(-1)) / overlappingArrBak.length) * width) + "%";
                 divNew.style.zIndex = 0;
             }
         } else {
