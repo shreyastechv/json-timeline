a53d2d9e2b5791a154bb948cc8924a4305799f1b
Shreyas A S <shreyas.as@techversantinfotech.com>
2024-10-24 17:02:19 +0530

    Rename a variable and add new one to make code more readable

--- js/script.js
+++ js/script.js
@@ -45,23 +45,23 @@ for (let i=0; i<data.length; i++) {
 
     const overlappingArr = [];
     let width = 100;
-    const currObj = data[i];
+    const currFirstObj = data[i];
 
     do {
         overlappingArr.unshift(i);
         i++;
-    } while (i < data.length && ((currObj.start + currObj.duration) > data[i].start));
+    } while (i < data.length && ((currFirstObj.start + currFirstObj.duration) > data[i].start));
     i--;
 
     for (let j=0; j<overlappingArr.length; j++) {
         const divNew = document.createElement("div");
-        divNew.style.marginLeft = (100/overlappingArr.length * j) + "%";
-        divNew.style.top = data[overlappingArr[j]].start + "px";
-        divNew.style.height = data[overlappingArr[j]].duration + "px";
-        divNew.textContent = data[overlappingArr[j]].title;
-        const bigArr = overlappingArrBak.filter((value) => data[value].start + data[value].duration > data[overlappingArr[j]].start);
+        const currObj = data[overlappingArr[j]];
+        divNew.style.top = currObj.start + "px";
+        divNew.style.height = currObj.duration + "px";
+        divNew.textContent = currObj.title;
+        const bigArr = overlappingArrBak.filter((value) => data[value].start + data[value].duration > currObj.start);
         if (bigArr.length !== 0) {
-            if (j ==0 && overlappingArr.length > 1 && Math.max(...overlappingArrBak) + 1 == Math.min(...overlappingArr)) {
+            if (j==0 && overlappingArr.length > 1 && Math.max(...overlappingArrBak) + 1 == Math.min(...overlappingArr)) {
                 width = width / 2;
             }
             let prevBigDiv;
@@ -74,6 +74,7 @@ for (let i=0; i<data.length; i++) {
             divNew.style.marginLeft = (parseInt(prevBigDiv.style.width) + parseInt(prevBigDiv.style.marginLeft)) + "%";
             divNew.style.width = (((overlappingArrBak.indexOf(bigArr.at(-1))+1) / overlappingArrBak.length) * width) + "%";
         } else {
+            divNew.style.marginLeft = (100/overlappingArr.length * j) + "%";
             divNew.style.width = 100/overlappingArr.length + "%";
         }
         document.getElementById("timeline-overlay").appendChild(divNew);
